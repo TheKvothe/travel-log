0bb480c0a345bdc27f17615da480a72d534edde2
Arthur Bernal <arthurbernal@MacBook-Pro-de-Arthur.local>
2020-10-30 16:05:20 +0100

    MongoDB via mongoose connection added, and Post endpoint added

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,37 @@
-const express = require ('express');
+const express = require('express');
 const morgan = require('morgan');
-const helmet = require ('helmet');
-const cors = require ('cors');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const middlewares = require('./middlewares');
+const logs = require('./api/logs');
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.use(morgan('common'));
 // helmet adds headers to the response from server
 // helmet eliminates the x-powered-by header (safer)
 app.use(helmet());
 app.use(cors({
-  origin: 'https://localhost:3000',
+  origin: 'process.env.CORS_ORIGIN',
 }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
     message: 'hello world',
   });
 });
+
+app.use('/api/logs', logs);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
