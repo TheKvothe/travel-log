9dcbc6e2445f6227919f4a90001cb5dc852b30bd
Arthur Bernal <arthurbernal@MacBook-Pro-de-Arthur.local>
2020-10-30 12:39:58 +0100

    remove Jenkinsfile

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const helmet = require ('helmet');
 const cors = require ('cors');
 
+const middlewares = require('./middlewares');
+
 const app = express();
 app.use(morgan('common'));
 // helmet adds headers to the response from server
@@ -18,21 +20,9 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use((req, res, next) => {
-  const error = new Error(`not found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
-// eslint-disable-next-line no-unused-vars
-app.use((error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '🐸' : error.stack,
-  });
-});
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
