03272912eba009a80c3f2b1676ee1cc823e31708
Arthur Bernal <arthurbernal@MacBook-Pro-de-Arthur.local>
2020-11-11 08:47:05 +0100

    clone del correcto

--- server/src/middlewares.js
+++ server/src/middlewares.js
@@ -1,5 +1,5 @@
 const notFound = (req, res, next) => {
-  const error = new Error(`not found - ${req.originalUrl}`);
+  const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
@@ -10,7 +10,7 @@ const errorHandler = (error, req, res, next) => {
   res.status(statusCode);
   res.json({
     message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '🐸' : error.stack,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
   });
 };
 
